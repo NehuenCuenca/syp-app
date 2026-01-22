a8ad8253facaa51112d04e420dd37ac2d312cc97
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2026-01-22 17:29:08 -0300

    fix(export-order): concatenate code with product name and remove discount column. Recalculate cells after remove columns

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -45,11 +45,11 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
     public function headings(): array
     {
         return [
-            'COD',
+            // 'COD',
             'Producto', 
             'Cantidad',
             'Precio unidad',
-            'Descuento (%)',
+            // 'Descuento',
             'Subtotal'
         ];
     }
@@ -59,12 +59,14 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
      */
     public function map($row): array
     {
+        // dd($row);
+        $productNameWithCode = "{$row->code}| {$row->product_name}";
         return [
-            $row->code,
-            $row->product_name,
+            // $row->code,
+            $productNameWithCode,
             number_format($row->quantity, 0),
             '$' . number_format($row->unit_price, 0),
-            number_format(($row->percentage_applied), 0) . '%',
+            // number_format(($row->percentage_applied), 0) . '%',
             '$' . number_format($row->line_subtotal, 0)
         ];
     }
@@ -88,7 +90,7 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
         $currentRow = 1;
         // Título principal
         $sheet->setCellValue('A1', $this->ticketType);
-        $sheet->mergeCells('A1:F1');
+        $sheet->mergeCells('A1:D1');
 
         // Agregar encabezado si está habilitado
         if ($this->includeHeader) {
@@ -132,26 +134,26 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
         $totalRow = $tableDataEndRow + 5; // Fila del total
         
         // Agregar fila de subtotal
-        $sheet->setCellValue("E{$subTotalRow}", 'SUBTOTAL:');
-        $sheet->setCellValue("F{$subTotalRow}", '$' . number_format($order->subtotal, 0));
+        $sheet->setCellValue("C{$subTotalRow}", 'SUBTOTAL:');
+        $sheet->setCellValue("D{$subTotalRow}", '$' . number_format($order->subtotal, 0));
         
         $currentRow++;
 
         // Agregar fila de ajuste
-        $sheet->setCellValue("E{$adjustmentRow}", 'AJUSTE:');
-        $sheet->setCellValue("F{$adjustmentRow}", '$' . number_format($order->adjustment_amount, 0));
+        $sheet->setCellValue("C{$adjustmentRow}", 'AJUSTE:');
+        $sheet->setCellValue("D{$adjustmentRow}", '$' . number_format($order->adjustment_amount, 0));
         
         $currentRow++;
 
         // Agregar fila de total
-        $sheet->setCellValue("E{$totalRow}", 'TOTAL NETO:');
-        $sheet->setCellValue("F{$totalRow}", '$' . number_format($order->total_net, 0));
+        $sheet->setCellValue("C{$totalRow}", 'TOTAL NETO:');
+        $sheet->setCellValue("D{$totalRow}", '$' . number_format($order->total_net, 0));
 
         // APLICAR ESTILOS
         $styles = [];
 
-            // Título principal
-        $styles['A1:F1'] = [
+        // Título principal
+        $styles['A1:D1'] = [
             'font' => [
                 'bold' => true,
                 'size' => 16,
@@ -183,7 +185,7 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
         }
 
         // Encabezados de la tabla
-        $styles["A{$tableHeaderRow}:F{$tableHeaderRow}"] = [
+        $styles["A{$tableHeaderRow}:D{$tableHeaderRow}"] = [
             'font' => [
                 'bold' => true,
                 'color' => ['argb' => 'FFFFFFFF']
@@ -204,7 +206,7 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
 
         // Datos de la tabla
         if ($tableDataEndRow >= $tableDataStartRow) {
-            $styles["A{$tableDataStartRow}:F{$tableDataEndRow}"] = [
+            $styles["A{$tableDataStartRow}:D{$tableDataEndRow}"] = [
                 'borders' => [
                     'allBorders' => [
                         'borderStyle' => Border::BORDER_THIN,
@@ -216,7 +218,7 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
             ];
 
             // Columnas de números alineadas a la derecha
-            $styles["D{$tableDataStartRow}:F{$tableDataEndRow}"] = [
+            $styles["C{$tableDataStartRow}:D{$tableDataEndRow}"] = [
                 'alignment' => [
                     'horizontal' => Alignment::HORIZONTAL_RIGHT,
                 ]
@@ -224,7 +226,7 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
         }
 
         // Fila del subtotal
-        $styles["E{$subTotalRow}:F{$subTotalRow}"] = [
+        $styles["C{$subTotalRow}:D{$subTotalRow}"] = [
             'font' => [
                 // 'bold' => true,
                 'size' => 12
@@ -244,7 +246,7 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
         ];
 
         // Fila del ajuste
-        $styles["E{$adjustmentRow}:F{$adjustmentRow}"] = [
+        $styles["C{$adjustmentRow}:D{$adjustmentRow}"] = [
             'font' => [
                 // 'bold' => true,
                 'size' => 12
@@ -264,7 +266,7 @@ class OrderExport implements FromCollection, WithHeadings, WithMapping, WithStyl
         ];
 
         // Fila del total
-        $styles["E{$totalRow}:F{$totalRow}"] = [
+        $styles["C{$totalRow}:D{$totalRow}"] = [
             'font' => [
                 'bold' => true,
                 'size' => 18
