6743e2d6773af88452e82b01922c88e07b662966
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2026-02-19 11:58:22 -0300

    fix(movements): replace type of name colum to enum and use MovementTypes constants

--- database/migrations/2024_01_10_000003_create_movement_types_table.php
+++ database/migrations/2024_01_10_000003_create_movement_types_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\MovementType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('movement_types', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); //'Compra', 'Venta', 'Ajuste_Positivo', 'Ajuste_Negativo'
+            $table->enum('name', MovementType::getMovementTypes());
             $table->boolean('increase_stock');
             $table->timestamps();
         });
