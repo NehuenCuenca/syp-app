64fb08880199b5c4a1ec0e553fa5c71fd981f0d4
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2026-02-19 19:17:33 -0300

    fix(contacts): add lenght limit to columns migration

--- database/migrations/2024_02_14_000000_create_contacts_table.php
+++ database/migrations/2024_02_14_000000_create_contacts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Contact;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,12 +14,13 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
-            $table->string('code')->unique()->nullable();
-            $table->string('name');
-            $table->string('email')->nullable();
-            $table->string('phone')->nullable();
-            $table->text('address')->nullable();
-            $table->enum('contact_type', ['Cliente', 'Proveedor']);
+            $table->string('code', 10)->unique()->nullable();
+            $table->string('name', 100);
+            $table->string('email', 100)->nullable();
+            $table->string('phone', 50)->nullable();
+            $table->text('address', 100)->nullable();
+            $table->enum('contact_type', Contact::getContactTypes())
+                    ->default(Contact::CONTACT_TYPE_CLIENT);
             $table->timestamps();
             $table->softDeletesTz();
         });
