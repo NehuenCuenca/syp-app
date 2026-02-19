1364f8141e899bf74cc78626c0e7ec6873b833b1
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2026-02-19 19:39:46 -0300

    feat(contacts): add form request to filter contacts

---
+++ app/Http/Requests/FilterContactsRequest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Contact;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\Rule;
+
+class FilterContactsRequest extends BaseApiRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->user()->tokenCan('server:read', Contact::class);
+    }
+
+    public const ALLOWED_SORT_FIELDS  = [
+        'name' => 'Nombre de negocio',
+        'contact_type' => 'Tipo de contacto',
+        'created_at' => 'Fecha de creación',
+        'deleted_at' => 'Fecha de eliminacion',
+    ];
+
+    public const ALLOWED_SORT_DIRECTIONS = [
+        'asc' => 'Ascendente',
+        'desc' => 'Descendente'
+    ];
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'per_page' => 'nullable|integer|min:1|max:100',
+            'page' => 'nullable|integer|min:1',
+            'sort_by' => [
+                'nullable',
+                'string',
+                Rule::in(array_keys(self::ALLOWED_SORT_FIELDS))
+            ],
+            'sort_direction' => ['nullable', 'string', Rule::in(array_keys(self::ALLOWED_SORT_DIRECTIONS))],
+            'contact_type' => ['nullable', 'string', Rule::in(Contact::getContactTypes())],
+            'search' => 'nullable|string|max:255',
+        ];
+    }
+
+    /**
+     * Get sanitized and processed data
+     *
+     * @return array
+     */
+    public function getFilters(): array
+    {
+        return [
+            'contact_type' => $this->input('contact_type'),
+            'search' => $this->input('search'),
+            'sort_by' => $this->input('sort_by', 'deleted_at'),
+            'sort_direction' => $this->input('sort_direction', 'asc'),
+            'per_page' => $this->integer('per_page', 9),
+            'page' => $this->integer('page', 1)
+        ];
+    }
+}
